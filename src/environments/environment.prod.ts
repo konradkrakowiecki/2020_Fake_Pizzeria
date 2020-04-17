b94e12b08071c56facf188acb538ea924b03e413
konrad.krakowiecki <konrad.krakowiecki@gmail.com>
2020-04-17 16:50:03 +0200

    Adding simple app routing, simple app structure and firebase extensions

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCQp-mT1wMmf9DSx4OE8ESTMZxu8q536sw',
+    authDomain: 'fake-pizzeria.firebaseapp.com',
+    databaseURL: 'https://fake-pizzeria.firebaseio.com',
+    projectId: 'fake-pizzeria',
+    storageBucket: 'fake-pizzeria.appspot.com',
+    messagingSenderId: '87770414101',
+    appId: '1:87770414101:web:077650b42069b46509621c',
+    measurementId: 'G-RTDDNKFNQR'
+
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCQp-mT1wMmf9DSx4OE8ESTMZxu8q536sw',
+    authDomain: 'fake-pizzeria.firebaseapp.com',
+    databaseURL: 'https://fake-pizzeria.firebaseio.com',
+    projectId: 'fake-pizzeria',
+    storageBucket: 'fake-pizzeria.appspot.com',
+    messagingSenderId: '87770414101',
+    appId: '1:87770414101:web:077650b42069b46509621c',
+    measurementId: 'G-RTDDNKFNQR'
+  }
 };
 
 /*
